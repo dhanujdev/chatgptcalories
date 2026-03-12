47e224c5ccccb069e433621a7457eecace95fc0d
dhanujdev <dhanujg499@gmail.com>
2026-03-12 00:22:55 -0400

    Bump widget template URI to v8 for cache busting

--- server/src/toolMetadata.ts
+++ server/src/toolMetadata.ts
@@ -1,4 +1,4 @@
-export const WIDGET_URI = "ui://widget/calorie-command-v7.html";
+export const WIDGET_URI = "ui://widget/calorie-command-v8.html";
 
 export function openDashboardToolMeta(widgetUri: string = WIDGET_URI) {
   return {
